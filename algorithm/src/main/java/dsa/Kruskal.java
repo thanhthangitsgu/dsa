285746b27889c9dbff6eeb87a2e42b80c2f8b0d7
thanhthangitsgu <thanhthang.itsgu@gmail.com>
2024-08-30 06:48:37 +0700

    update

--- algorithm/src/main/java/dsa/Kruskal.java
+++ algorithm/src/main/java/dsa/Kruskal.java
@@ -1,6 +1,25 @@
 package dsa;
 
 public class Kruskal {
+    int find(int u, int parent[]) {
+        if (parent[u] == u) {
+            return u;
+        }
+
+        return parent[u] = find(parent[u], parent);
+    }
+
+    boolean join(int u, int v, int parent[]) {
+        u = find(u, parent);
+        v = find(v, parent);
+
+        if (u != v) {
+            parent[v] = u;
+            return true;
+        }
+
+        return false;
+    }
 
     public void implementAlgorithm() {
 
