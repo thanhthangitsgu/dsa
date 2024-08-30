8acd1a9f2d746f1424ccd4b9cc645a5cbef71ba2
thangpt <thangpt@cnv.vn>
2024-08-30 08:45:42 +0700

    update

--- algorithm/src/main/java/dsa/Main.java
+++ algorithm/src/main/java/dsa/Main.java
@@ -7,7 +7,7 @@ import dsa.supporter.FileReader;
 public class Main {
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-        Graph graph = fileReader.readGraphFromFile("D:/Project/dsa/domain/src/main/java/dsa/asset/graph-9.txt");
+        Graph graph = fileReader.readGraphFromFile("/Users/thanhthang/Desktop/Project/dsa/domain/src/main/java/dsa/asset/graph-9.txt");
 
         Dijkstra dijkstra = new Dijkstra();
         dijkstra.implementAlgorithm(graph);
--- algorithm/src/main/java/dsa/Prim.java
+++ algorithm/src/main/java/dsa/Prim.java
@@ -4,6 +4,9 @@ import dsa.graph.Graph;
 
 public class Prim {
     public void implementAlgorithm(Graph graph) {
+        int size = graph.getSize();
+        int[] cost = new int[size];
+
 
     }
 }
